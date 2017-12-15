de02588ee172519e4c5e92f294cf7fad528322c3
raju1488 <rshanmug14@gmail.com>
2017-12-15 12:13:51 +0530

    Add files via upload

---
+++ setup/Binaries/OpenCover/opencover.4.6.519/sample - Copy.cmd
@@ -0,0 +1,3 @@
+OpenCover.Console.exe -register:user "-target:C:\Windows\System32\inetsrv\w3wp.exe" -targetargs:-debug -targetdir:"D:\WebClients\code\WebClient\bin" -filter:+[*]* -output:output.xml
+
+pause aspnet_regiis.exe -GA ldo\xkmunirathina
